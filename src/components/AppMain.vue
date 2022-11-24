dfefe20dc9441f1f65a5c8b16480815ed6e5d7b6
NicholasPeara92 <nicholas.peara@gmail.com>
2022-11-24 16:46:08 +0100

    Aggiunge main e pre footer

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,10 +16,20 @@ export default {
 </script>
 
 <template>
-  <h1>Main</h1>
+  <main>
+    <div class="container">
+      <h2>-- Content goes here -- </h2>
+    </div>
+  </main>
   <AppPrefooter/>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  background-color: var(--secondary-color);
+  color: white;
+  height: 8.25rem;
+}
+</style>
 
 
--- src/components/AppPrefooter.vue
+++ src/components/AppPrefooter.vue
@@ -3,14 +3,76 @@ export default {
   name: 'AppPrefooter',
   data() {
     return {
-      
+      links: [
+        'DIGITAL COMICS',
+        'DC MERCHANDISE',
+        'SUBSCRIPTION',
+        'COMIC SHOP LOCATOR',
+        'DC POWER VISA',
+      ]
     }
   }
 }
 </script>
 
 <template>
-  <h1>PreFooter</h1>
+  <section>
+    <div class="container">
+      <ul>
+        <li>
+          <img src="../assets/buy-comics-digital-comics.png" alt="">
+          <a href="#">DIGITAL COMICS</a>
+        </li>
+        <li>
+          <img src="../assets/buy-comics-merchandise.png" alt="">
+          <a href="#">DC MERCHANDISE</a>
+        </li>
+        <li>
+          <img src="../assets/buy-comics-subscriptions.png" alt="">
+          <a href="#">SUBSCRIPTION</a>
+        </li>
+        <li>
+          <img src="../assetS/buy-comics-shop-locator.png" alt="">
+          <a href="#">COMIC SHOP LOCATOR</a>
+        </li>
+        <li>
+          <img src="../assets/buy-dc-power-visa.svg" alt="">
+          <a href="#">DC POWER VISA</a>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+section {
+  background-color: var(--primary-color);
+  height: 9.6875rem;
+  display: flex;
+  align-items: center;
+}
+.container {
+  display: flex;
+  ul {
+    display: flex;
+    list-style: none;
+    justify-content: space-between;
+    li {
+      display: flex;
+      align-items: center;
+      margin: 0 20px;
+      img {
+        height: 3.4375rem;
+  
+      }
+      a {
+        display: inline-block;
+        padding: 20px 10px;
+        text-decoration: none;
+        color: white;
+        font-weight: bold;
+      }
+    }
+  }
+}
+</style>
